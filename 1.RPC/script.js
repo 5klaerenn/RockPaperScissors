0c07ec681fd8f930eb9f71b1b2642df251cb09b0
B0bSnorkel <bob.snorkel@outlook.fr>
2022-06-24 14:10:37 -0400

    Algorithme simple pour un seul tour

--- 1.RPC/script.js
+++ 1.RPC/script.js
@@ -9,12 +9,43 @@
 
 */
 
-let choices = ["rock", "paper", "scissors"]
+let userChoice = prompt("Rock, Paper or Scissors ?").toUpperCase();
 
 function computerPlay() {
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice
+    let choices = ["ROCK", "PAPER", "SCISSORS"]
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+}
+
+let randomChoice = computerPlay(); 
+
+function playRound(userChoice, randomChoice) {
+    if (userChoice == randomChoice) {
+        return  "It's a Tie ! Try again"; 
+    } else if (userChoice == "ROCK") {
+        if (randomChoice == "PAPER") {
+            return "You Lose ! Paper beats rock";
+        } else {
+            return "You Win ! Rock beats Scissors";
+        } 
+    } else if (userChoice == "PAPER") {
+        if (randomChoice == "SCISSORS") {
+            return "You Lose ! Scissors beat Paper";
+        } else {
+            return "You Win ! Paper beats rock";
+        } 
+    } else if (userChoice == "SCISSORS") {
+        if (randomChoice == "ROCK") {
+            return "You Lose ! Rock beats Scissors";
+        } else {
+            return "You Win ! Scissors beat Paper";
+        } 
+    } else {
+        return "Oops! Something's wrong";
+    }
 }
 
 
-console.log(computerPlay())
+console.log(userChoice);
+console.log(randomChoice);
+console.log(playRound(userChoice, randomChoice)); 
